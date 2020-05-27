8a6208669b03288f0b89234d381e736dfff601bd
kyuu090 <k.on13pose@gmail.com>
2020-05-27 23:55:47 +0900

    タイポ

--- bot.js
+++ bot.js
@@ -2,7 +2,7 @@ module.exports = class Bot {
     constructor(client) {
         this.client = client;
     }
-    recieve = async (event) => {
+    receive = async (event) => {
         return this.client.replyMessage(event.replyToken, {
             type:"text",
             text:"筋トレしたか？？？？？"
